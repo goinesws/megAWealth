2459e12c1dd51b822540c9726aa24d66fe2913a3
goinesws <64315827+goinesws@users.noreply.github.com>
2022-06-22 23:39:08 +0700

    models, migrations

---
+++ database/migrations/2022_06_22_162103_create_offices_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOfficesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('offices', function (Blueprint $table) {
+            $table->id();
+            $table->string('address');
+            $table->string('contact_name');
+            $table->string('phone_number');
+            $table->string('image_link');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('offices');
+    }
+}
---
+++ database/migrations/2022_06_22_162212_create_estates_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEstatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('estates', function (Blueprint $table) {
+            $table->id();
+            $table->string('sales_type');
+            $table->string('building_type');
+            $table->integer('price');
+            $table->string('location');
+            $table->string('image_link');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('estates');
+    }
+}
