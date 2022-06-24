baa230fb3aca81b1c2858a1fe206b6770481d9c1
goinesws <64315827+goinesws@users.noreply.github.com>
2022-06-24 16:09:40 +0700

    route

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use app\Http\Controllers\UserController;
-use app\Http\Controllers\Controller;
-use app\Http\Controllers\CartController;
+
 
 
 
@@ -23,34 +22,4 @@ Route::get('/', function () {
 });
 
 
-Route::get('/login', [UserController::class, 'index_login']);
-Route::post('/login', [UserController::class, 'login']);
-
-
 Route::get('/register', [UserController::class, 'index_register']);
-Route::post('/register', [UserController::class, 'register']);
-
-Route::get('logout', [UserController::class, 'logout']);
-
-
-Route::get('/home', [Controller::class, 'home']);
-
-Route::get('/search', [Controller::class, 'search']);
-
-
-//guest and member
-//about us
-Route::get('/about', [Controller::class, 'about']);
-
-//buy
-Route::get('/buy', [Controller::class, 'buy']);
-
-//rent
-Route::get('/rent', [Controller::class, 'rent']);
-
-//cart
-Route::get('/addcart', [CartController::class, 'addcart']);
-
-
-// Route::get('/addcart', [CartController::class, 'addcart']);
-
