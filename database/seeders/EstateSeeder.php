0bd28843a3bd6e25fe7e6db9613bf391600afead
Cindy <cindywijaya919@gmail.com>
2022-06-29 15:14:09 +0700

    estate_seeding

--- database/seeders/EstateSeeder.php
+++ database/seeders/EstateSeeder.php
@@ -35,5 +35,96 @@ class EstateSeeder extends Seeder
             'location'=>'2059 James Street, NY',
             'image_link'=>'apartment_1.jpg'
         ]);
+
+        Estate::create([
+            'estate_id'=> Str::uuid(),
+            'sales_type'=> 'Rent',
+            'building_type'=> 'Apartment',
+            'status'=> 'Open',
+            'price'=> '2500000',
+            'location'=>'3054 Deans Lane, NY',
+            'image_link'=>'apartment_2.jpg'
+        ]);
+
+        Estate::create([
+            'estate_id'=> Str::uuid(),
+            'sales_type'=> 'Sale',
+            'building_type'=> 'Apartment',
+            'status'=> 'Open',
+            'price'=> '5800000',
+            'location'=>' 813 Howard Street, USA',
+            'image_link'=>'apartment_3.jpg'
+        ]);
+
+        Estate::create([
+            'estate_id'=> Str::uuid(),
+            'sales_type'=> 'Sale',
+            'building_type'=> 'House',
+            'status'=> 'Cart',
+            'price'=> '2300000',
+            'location'=>'442 Hamilton Drive, WA',
+            'image_link'=>'house_2.jpg'
+        ]);
+
+        Estate::create([
+            'estate_id'=> Str::uuid(),
+            'sales_type'=> 'Rent',
+            'building_type'=> 'House',
+            'status'=> 'Open',
+            'price'=> '180000',
+            'location'=>'1101 Angus Road, NY',
+            'image_link'=>'house_3.jpg'
+        ]);
+
+        Estate::create([
+            'estate_id'=> Str::uuid(),
+            'sales_type'=> 'Rent',
+            'building_type'=> 'House',
+            'status'=> 'Transaction Complete',
+            'price'=> '120000',
+            'location'=>'4803 Wellesley Street, ON',
+            'image_link'=>'house_4.jpg'
+        ]);
+
+        Estate::create([
+            'estate_id'=> Str::uuid(),
+            'sales_type'=> 'Sale',
+            'building_type'=> 'Apartment',
+            'status'=> 'Open',
+            'price'=> '6600000',
+            'location'=>'207 Queens Quay West, ON',
+            'image_link'=>'apartment_4.jpg'
+        ]);
+
+        Estate::create([
+            'estate_id'=> Str::uuid(),
+            'sales_type'=> 'Rent',
+            'building_type'=> 'Apartment',
+            'status'=> 'Cart',
+            'price'=> '45000',
+            'location'=>'2456 Ella Street, CA',
+            'image_link'=>'apartment_5.jpg'
+        ]);
+
+        Estate::create([
+            'estate_id'=> Str::uuid(),
+            'sales_type'=> 'Rent',
+            'building_type'=> 'Apartment',
+            'status'=> 'Open',
+            'price'=> '50000',
+            'location'=>'8028 Ella Street, CA',
+            'image_link'=>'apartment_6.jpg'
+        ]);
+
+        Estate::create([
+            'estate_id'=> Str::uuid(),
+            'sales_type'=> 'Rent',
+            'building_type'=> 'Apartment',
+            'status'=> 'Open',
+            'price'=> '9500000',
+            'location'=>'4561 Flinderation Road, IL',
+            'image_link'=>'apartment_7.png'
+        ]);
+
     }
 }
